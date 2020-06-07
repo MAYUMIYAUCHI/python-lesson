e3cbae105eb40e1a23fdfc5ee8ac69b8e76bc26f
阿部直也 <n.abe@gemcook.com>
2020-06-07 15:12:36 +0900

    Fixed indent.

--- testt.py
+++ testt.py
@@ -1,8 +1,9 @@
 count = 0
 while True:
-  if count >= 5:
-    break
-   print(count)
-  #print(count)
-  count += 1
-  #print(count)
+    if count >= 5:
+        break
+
+    print(count)
+    # print(count)
+    count += 1
+    # print(count)
